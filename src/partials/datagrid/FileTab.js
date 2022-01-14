dc71a9fce54029a84f2bc9fade67f0c0e6b9d494
James Murdock Graham <jamesmurdockgraham@gmail.com>
2022-01-14 13:47:10 -0100

    added x by default

--- src/partials/datagrid/FileTab.js
+++ src/partials/datagrid/FileTab.js
@@ -8,7 +8,7 @@ export const FileTab = ({ selectFile, closeFile, item, selectedFile }) => {
   return (
     <div
       onClick={() => selectFile(item)}
-      className={`flex relative cursor-pointer group hover:bg-gray-600 items-center ${
+      className={`flex relative cursor-pointer hover:bg-gray-600 items-center ${
         selectedFile === item
           ? "border border-blue-600"
           : "border border-gray-600"
@@ -16,7 +16,7 @@ export const FileTab = ({ selectFile, closeFile, item, selectedFile }) => {
     >
       <span className="text-yellow-500 mr-2 text-xs font-bold">CSV</span>
       {item}
-      <div className="rounded-full bg-gray-500 hidden group-hover:flex h-4 absolute right-0 mr-2 z-60">
+      <div className="h-4 w-4 ml-2">
         <XIcon onClick={handleClose} />
       </div>
     </div>
