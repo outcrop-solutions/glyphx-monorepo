eb61d053ed79692d1aeeada4a47cd19842e05ed5
JP Burford <jp.burford@h1insights.com>
2022-12-21 16:27:56 -0500

    adding unit tests for ForkingStream

---
+++ packages/fileIngestion/src/__tests__/fileProcessing/fileReconciliator.tests.ts
@@ -0,0 +1,6 @@
+import {assert} from 'chai';
+import {FileReconciliator} from '@fileProcessing';
+
+describe('#fileProcessing/FileReconciliator', () => {
+  context('reconcileFileInformation', () => {});
+});
