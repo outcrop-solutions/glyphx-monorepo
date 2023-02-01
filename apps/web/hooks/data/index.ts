6e11890df5523ca0ce8c30753a17c52ca33e012e
James Murdock Graham <jamesmurdockgraham@gmail.com>
2023-02-01 13:21:23 -0500

    moved hooks back into nextjs app

--- apps/web/hooks/data/index.ts
+++ apps/web/hooks/data/index.ts
@@ -2,4 +2,4 @@ import useInvitations from './useInvitations';
 import useMembers from './useMembers';
 import useWorkspaces from './useWorkspaces';
 
-export { useInvitations, useMembers, useWorkspaces };
+export {useInvitations, useMembers, useWorkspaces};
