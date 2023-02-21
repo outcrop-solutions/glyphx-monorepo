93f3cf5903b00c480779a3ba22ce75545769321a
jp-burford <48831852+jp-burford@users.noreply.github.com>
2023-02-21 09:54:05 -0500

    Fixing linting issues on types package. (#19)
    Co-authored-by: JP Burford <jp.burford@h1insights.com>

--- packages/types/tsup.config.ts
+++ packages/types/tsup.config.ts
@@ -1,12 +1,12 @@
 import {defineConfig} from 'tsup';
 
-const isProduction = process.env.NODE_ENV === 'production';
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   clean: true,
   dts: true,
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  minify: isProduction,
+  minify: IS_PRODUCTION,
   sourcemap: true,
 });
