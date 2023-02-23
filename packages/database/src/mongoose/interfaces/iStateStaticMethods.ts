c84c201c1f0ba043189884970849c767af0ef709
James Graham <51002460+james-murdock-graham@users.noreply.github.com>
2023-02-23 13:21:29 -0500

    fixed IQueryresult imports (#42)

--- packages/database/src/mongoose/interfaces/iStateStaticMethods.ts
+++ packages/database/src/mongoose/interfaces/iStateStaticMethods.ts
@@ -1,5 +1,5 @@
-import {IQueryResult, Types as mongooseTypes, Model} from 'mongoose';
-import {database as databaseTypes} from '@glyphx/types';
+import {Types as mongooseTypes, Model} from 'mongoose';
+import {IQueryResult, database as databaseTypes} from '@glyphx/types';
 import {IStateMethods} from './iStateMethods';
 export interface IStateStaticMethods
   extends Model<databaseTypes.IState, {}, IStateMethods> {
