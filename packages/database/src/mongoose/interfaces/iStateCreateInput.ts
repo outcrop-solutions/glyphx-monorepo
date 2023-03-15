f53eebbe537b3abf61b0cdc147d7a905afcae158
James Murdock Graham <jamesmurdockgraham@gmail.com>
2023-03-15 11:39:20 -0500

    updated interface exports

--- packages/database/src/mongoose/interfaces/iStateCreateInput.ts
+++ packages/database/src/mongoose/interfaces/iStateCreateInput.ts
@@ -1,7 +1,7 @@
 import {database as databaseTypes} from '@glyphx/types';
 import {Types as mongooseTypes} from 'mongoose';
 
-export interface IStateDocument
+export interface IStateCreateInput
   extends Omit<
     databaseTypes.IState,
     '_id' | 'createdAd' | 'updatedAt' | 'projects'
--- packages/database/src/mongoose/interfaces/index.ts
+++ packages/database/src/mongoose/interfaces/index.ts
@@ -1,36 +1,68 @@
-export type {IUserStaticMethods} from './iUserStaticMethods';
-export type {IUserMethods} from './iUserMethods';
-export type {IUserDocument} from './iUserDocument';
-export type {ICustomerPaymentStaticMethods} from './iCustomerPaymentStaticMethods';
-export type {ICustomerPaymentMethods} from './iCustomerPaymentMethods';
-export type {ICustomerPaymentDocument} from './iCustomerPaymentDocument';
-export type {IMemberStaticMethods} from './iMemberStaticMethods';
-export type {IMemberMethods} from './iMemberMethods';
-export type {IMemberDocument} from './iMemberDocument';
-export type {IVerificationTokenStaticMethods} from './iVerificationTokenStaticMethods';
-export type {IVerificationTokenMethods} from './iVerificationTokenMethods';
-export type {IVerificationTokenDocument} from './iVerificationTokenDocument';
+// Accounts
+export type {IAccountCreateInput} from './iAccountCreateInput';
 export type {IAccountDocument} from './iAccountDocument';
 export type {IAccountMethods} from './iAccountMethods';
 export type {IAccountStaticMethods} from './iAccountStaticMethods';
-export type {IWorkspaceDocument} from './iWorkspaceDocument';
-export type {IWorkspaceMethods} from './iWorkspaceMethods';
-export type {IWorkspaceStaticMethods} from './iWorkspaceStaticMethods';
-export type {IProjectDocument} from './iProjectDocument';
-export type {IProjectMethods} from './iProjectMethods';
-export type {IProjectStaticMethods} from './iProjectStaticMethods';
-export type {IProjectTypeDocument} from './iProjectTypeDocument';
-export type {IProjectTypeMethods} from './iProjectTypeMethods';
-export type {IProjectTypeStaticMethods} from './iProjectTypeStaticMethods';
-export type {IStateDocument} from './iStateDocument';
-export type {IStateMethods} from './iStateMethods';
-export type {IStateStaticMethods} from './iStateStaticMethods';
-export type {ISessionDocument} from './iSessionDocument';
-export type {ISessionMethods} from './iSessionMethods';
-export type {ISessionStaticMethods} from './iSessionStaticMethods';
-export type {IWebhookDocument} from './iWebhookDocument';
-export type {IWebhookMethods} from './iWebhookMethods';
-export type {IWebhookStaticMethods} from './iWebhookStaticMethods';
+
+// CustomerPayment
+export type {ICustomerPaymentCreateInput} from './iCustomerPaymentCreateInput';
+export type {ICustomerPaymentDocument} from './iCustomerPaymentDocument';
+export type {ICustomerPaymentMethods} from './iCustomerPaymentMethods';
+export type {ICustomerPaymentStaticMethods} from './iCustomerPaymentStaticMethods';
+
+// Member
+export type {IMemberCreateInput} from './iMemberCreateInput';
+export type {IMemberDocument} from './iMemberDocument';
+export type {IMemberMethods} from './iMemberMethods';
+export type {IMemberStaticMethods} from './iMemberStaticMethods';
+
+// Process Tracking
 export type {IProcessTrackingDocument} from './iProcessTrackingDocument';
 export type {IProcessTrackingMethods} from './iProcessTrackingMethods';
 export type {IProcessTrackingStaticMethods} from './iProcessTrackingStaticMethods';
+
+// Project
+export type {IProjectCreateInput} from './iProjectCreateInput';
+export type {IProjectDocument} from './iProjectDocument';
+export type {IProjectMethods} from './iProjectMethods';
+export type {IProjectStaticMethods} from './iProjectStaticMethods';
+
+// ProjectType
+export type {IProjectTypeDocument} from './iProjectTypeDocument';
+export type {IProjectTypeMethods} from './iProjectTypeMethods';
+export type {IProjectTypeStaticMethods} from './iProjectTypeStaticMethods';
+
+// Session
+export type {ISessionCreateInput} from './iSessionCreateInput';
+export type {ISessionDocument} from './iSessionDocument';
+export type {ISessionMethods} from './iSessionMethods';
+export type {ISessionStaticMethods} from './iSessionStaticMethods';
+
+// State
+export type {IStateCreateInput} from './iStateCreateInput';
+export type {IStateDocument} from './iStateDocument';
+export type {IStateMethods} from './iStateMethods';
+export type {IStateStaticMethods} from './iStateStaticMethods';
+
+// User
+export type {IUserStaticMethods} from './iUserStaticMethods';
+export type {IUserDocument} from './iUserDocument';
+export type {IUserMethods} from './iUserMethods';
+export type {IUserCreateInput} from './iUserCreateInput';
+
+// Verification Token
+export type {IVerificationTokenStaticMethods} from './iVerificationTokenStaticMethods';
+export type {IVerificationTokenMethods} from './iVerificationTokenMethods';
+export type {IVerificationTokenDocument} from './iVerificationTokenDocument';
+
+// Webhook
+export type {IWebhookCreateInput} from './iWebhookCreateInput';
+export type {IWebhookDocument} from './iWebhookDocument';
+export type {IWebhookMethods} from './iWebhookMethods';
+export type {IWebhookStaticMethods} from './iWebhookStaticMethods';
+
+// Workspace
+export type {IWorkspaceCreateInput} from './iWorkspaceCreateInput';
+export type {IWorkspaceDocument} from './iWorkspaceDocument';
+export type {IWorkspaceMethods} from './iWorkspaceMethods';
+export type {IWorkspaceStaticMethods} from './iWorkspaceStaticMethods';
