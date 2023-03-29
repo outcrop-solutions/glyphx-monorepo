fce98a6b37ea90dc6fb96b2c0c73b8a0fc358ed7
James Murdock Graham <jamesmurdockgraham@gmail.com>
2023-03-29 15:27:33 -0500

    add comments for clarity

--- apps/web/src/state/index.tsx
+++
@@ -1,7 +0,0 @@
-export * from './comments';
-export * from './files';
-export * from './filters';
-export * from './project';
-export * from './properties';
-export * from './ui';
-export * from './workspace';
--- packages/types/src/web/constants/index.ts
+++ packages/types/src/web/constants/index.ts
@@ -1,7 +1,6 @@
 export * from './accepts';
 export * from './axis';
 export * from './directionType';
-export * from './filterType';
 export * from './http';
 export * from './ingestionType';
 export * from './interpolationType';
