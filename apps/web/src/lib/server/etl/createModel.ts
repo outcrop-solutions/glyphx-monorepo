eaf9eae523d6c37cb594448020ecba8ceb01ad85
James Murdock Graham <jamesmurdockgraham@gmail.com>
2023-04-06 12:28:32 -0500

    live glyph engine

--- apps/web/src/lib/server/etl/createModel.ts
+++ apps/web/src/lib/server/etl/createModel.ts
@@ -113,10 +113,10 @@ export const createModel = async (req: NextApiRequest, res: NextApiResponse) => 
     const properties = deepMerge.state.properties;
 
     const payload = {
-      // model_id: `deepMerge._id`,
-      model_id: `642ae3b1c976ba8cc7ac445e`,
-      // client_id: deepMerge.workspace,
-      client_id: 'testclientid02d78bf6f54f485f81295ec510841742',
+      model_id: deepMerge._id,
+      // model_id: `642ae3b1c976ba8cc7ac445e`,
+      client_id: deepMerge.workspace._id,
+      // client_id: 'testclientid02d78bf6f54f485f81295ec510841742',
       // filter: filter,
       x_axis: properties[webTypes.constants.AXIS.X]['key'],
       y_axis: properties[webTypes.constants.AXIS.Y]['key'],
@@ -129,39 +129,37 @@ export const createModel = async (req: NextApiRequest, res: NextApiResponse) => 
       z_direction: properties[webTypes.constants.AXIS.Z]['direction'],
     };
 
-    console.dir({ payload }, { depth: null });
-    res.status(200).json({ payload });
+    try {
+      // Setup process tracking
+      const PROCESS_ID = generalPurposeFunctions.processTracking.getProcessId();
+      const PROCESS_NAME = 'testingProcessUnique';
+      await processTrackingService.createProcessTracking(PROCESS_ID, PROCESS_NAME);
+
+      // construct GlyphEngine
+      const glyphEngine = new GlyphEngine(S3_BUCKET_NAME, S3_BUCKET_NAME, ATHENA_DB_NAME, PROCESS_ID);
+      await glyphEngine.init();
+
+      let data: Map<string, string>;
+      data = new Map<string, string>([
+        ['x_axis', payload['x_axis']],
+        ['y_axis', payload['y_axis']],
+        ['z_axis', payload['z_axis']],
+        ['x_func', payload['x_func']],
+        ['y_func', payload['y_func']],
+        ['z_func', payload['z_func']],
+        ['x_direction', payload['x_direction']],
+        ['y_direction', payload['y_direction']],
+        ['z_direction', payload['z_direction']],
+        ['model_id', payload['model_id']],
+        ['client_id', payload['client_id']],
+      ]);
+
+      // process glyph engine
+      const { sdtFileName, sgnFileName, sgcFileName } = await glyphEngine.process(data);
+
+      res.status(200).json({ data: { sdtFileName, sgnFileName, sgcFileName } });
+    } catch (error) {
+      res.status(404).json({ errors: { error: { msg: error.message } } });
+    }
   }
-  // try {
-  //   // Setup process tracking
-  //   const PROCESS_ID = generalPurposeFunctions.processTracking.getProcessId();
-  //   const PROCESS_NAME = 'testingProcessUnique';
-  //   await processTrackingService.createProcessTracking(PROCESS_ID, PROCESS_NAME);
-
-  //   // construct GlyphEngine
-  //   const glyphEngine = new GlyphEngine(S3_BUCKET_NAME, S3_BUCKET_NAME, ATHENA_DB_NAME, PROCESS_ID);
-  //   await glyphEngine.init();
-
-  //   let data: Map<string, string>;
-  //   data = new Map<string, string>([
-  //     ['x_axis', payload['x_axis']],
-  //     ['y_axis', payload['y_axis']],
-  //     ['z_axis', payload['z_axis']],
-  //     ['x_func', payload['x_func']],
-  //     ['y_func', payload['y_func']],
-  //     ['z_func', payload['z_func']],
-  //     ['x_direction', payload['x_direction']],
-  //     ['y_direction', payload['y_direction']],
-  //     ['z_direction', payload['z_direction']],
-  //     ['model_id', payload['model_id']],
-  //     ['client_id', payload['client_id']],
-  //   ]);
-
-  //   // process glyph engine
-  //   const { sdtFileName, sgnFileName, sgcFileName } = await glyphEngine.process(data);
-
-  //   res.status(200).json({ data: { sdtFileName, sgnFileName, sgcFileName } });
-  // } catch (error) {
-  //   res.status(404).json({ errors: { error: { msg: error.message } } });
-  // }
 };
