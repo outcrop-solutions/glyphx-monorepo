02087fecdbbeca2696aaed61c0a88d0f66ab7f29
James Murdock Graham <jamesmurdockgraham@gmail.com>
2023-04-10 12:09:29 -0500

    comment

--- apps/web/src/partials/comments/CommentsSidebar.tsx
+++ apps/web/src/partials/comments/CommentsSidebar.tsx
@@ -4,23 +4,22 @@ import { CommentInput } from './CommentInput';
 import { History } from './CommentHistory';
 import { useRecoilState } from 'recoil';
 
+// TODO: reinstate comments functionality
 export const CommentsSidebar = () => {
   return (
     <>
-      {true ? (
-        <div
-          id="sidebar"
-          className={`hidden lg:flex flex-col absolute z-10 right-0 top-0 lg:static border-l border-gray lg:right-auto lg:top-auto lg:translate-x-0 transform h-full scrollbar-none w-64 lg:w-20 lg:comments-sidebar-expanded:!w-64 shrink-0 transition-all duration-200 ease-in-out ${
-            true ? 'translate-y-64' : 'translate-x-0'
-          }`}
-        >
-          {/* <CommentsHeader sidebarExpanded={commentsOpen} /> */}
-          <div className="m-2 hidden comments-sidebar-expanded:block overflow-y-scroll scrollbar-thin scrollbar-thumb-yellow scrollbar-thumb-rounded-full">
-            {/* <History comments={comments} />
+      <div
+        id="sidebar"
+        className={`hidden lg:flex flex-col absolute z-10 right-0 top-0 lg:static border-l border-gray lg:right-auto lg:top-auto lg:translate-x-0 transform h-full scrollbar-none w-64 lg:w-20 lg:comments-sidebar-expanded:!w-64 shrink-0 transition-all duration-200 ease-in-out ${
+          true ? 'translate-y-64' : 'translate-x-0'
+        }`}
+      >
+        {/* <CommentsHeader sidebarExpanded={commentsOpen} /> */}
+        <div className="m-2 hidden comments-sidebar-expanded:block overflow-y-scroll scrollbar-thin scrollbar-thumb-yellow scrollbar-thumb-rounded-full">
+          {/* <History comments={comments} />
             <CommentInput setComments={setComments} /> */}
-          </div>
         </div>
-      ) : null}
+      </div>
     </>
   );
 };
