87587543a9fac650c8706977495d7ed988b3727b
James Murdock Graham <jamesmurdockgraham@gmail.com>
2023-04-12 14:12:37 -0500

    refactor stripe api routes to conform to pattern

--- apps/web/src/pages/api/payments/hooks.tsx
+++ apps/web/src/pages/api/payments/hooks.tsx
@@ -1,41 +1,15 @@
-import { buffer } from 'micro';
-
-import { StripeClient, customerPaymentService, Initializer } from '@glyphx/business';
+import { Initializer } from '@glyphx/business';
+import { stripeHooks } from 'lib/server';
 
 export const config = { api: { bodyParser: false } };
 
-const handler = async (req, res) => {
+const hooks = async (req, res) => {
+  // initialize the business layer
   if (!Initializer.initedField) {
     await Initializer.init();
   }
-
-  const reqBuffer = await buffer(req);
-  const signature = req.headers['stripe-signature'];
-  let event = null;
-
-  try {
-    event = StripeClient.stripe.webhooks.constructEvent(reqBuffer, signature, process.env.PAYMENTS_SIGNING_SECRET);
-  } catch (err) {
-    return res.status(400).send(`Webhook Error: ${err.message}`);
-  }
-
-  if (event) {
-    const { metadata } = event.data.object;
-
-    switch (event.type) {
-      case 'charge.succeeded':
-        if (metadata?.customerId && metadata?.type) {
-          await customerPaymentService.updateSubscription(metadata.customerId, metadata.type);
-        }
-        break;
-      default:
-        res.status(400).send(`Webhook Error: Unhandled event type ${event.type}`);
-    }
-  } else {
-    return res.status(400).send(`Webhook Error: Event not created`);
-  }
-
-  res.status(200).send({ received: true });
+  // execute the appropriate handler
+  return stripeHooks(req, res);
 };
 
-export default handler;
+export default hooks;
