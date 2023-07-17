49cdca955ec2d58731e27eea41cf0a213caa67fb
JP Burford <jp.burford@h1insights.com>
2023-07-17 09:26:53 -0400

    cleaning up Z axis

--- crates/cube_model/src/model/pipeline/model_artifacts/vertex_buffer.rs
+++ crates/cube_model/src/model/pipeline/model_artifacts/vertex_buffer.rs
@@ -1,5 +1,7 @@
+use crate::assets::glyph::{
+    build_x_oriented_glyph, build_y_oriented_glyph, build_z_oriented_glyph, Vertex,
+};
 use bytemuck;
-use crate::assets::glyph::{Vertex, build_x_oriented_glyph, build_y_oriented_glyph, build_z_oriented_glyph};
 
 pub struct VertexData {
     pub verticies: Vec<Vertex>,
@@ -18,21 +20,23 @@ impl VertexData {
     pub fn new() -> VertexData {
         let mut verticies: Vec<Vertex> = Vec::new();
         let mut indicies: Vec<u16> = Vec::new();
-        let (x_verticies, x_indicies) = build_x_oriented_glyph(-1.0, 1.0, -1.0, -1.0, 0.01, &[1.0, 0.0, 0.0]);
+        let (x_verticies, x_indicies) =
+            build_x_oriented_glyph(-1.0, 1.0, -1.0, -1.0, 0.01, &[1.0, 0.0, 0.0]);
         verticies.extend(x_verticies);
         indicies.extend(x_indicies);
 
-        let (y_verticies, y_indicies) = build_y_oriented_glyph(-1.0,1.0, -1.0, -1.0, 0.01, &[0.0, 1.0, 0.0]);
+        let (y_verticies, y_indicies) =
+            build_y_oriented_glyph(-1.0, 1.0, -1.0, -1.0, 0.01, &[0.0, 1.0, 0.0]);
 
         let offset = verticies.len() as u16;
         verticies.extend(y_verticies);
         indicies.extend(y_indicies.iter().map(|x| x + offset));
 
-        let (z_verticies, z_indicies) = build_z_oriented_glyph(-1.0,1.0 , -1.0, -1.0, 0.01, &[0.0, 0.0, 1.0]);
+        let (z_verticies, z_indicies) =
+            build_z_oriented_glyph(-1.0, 1.0, -1.0, -1.0, 0.01, &[0.0, 0.0, 1.0]);
         let offset = verticies.len() as u16;
         verticies.extend(z_verticies);
         indicies.extend(z_indicies.iter().map(|x| x + offset));
-
 
         VertexData {
             verticies,
