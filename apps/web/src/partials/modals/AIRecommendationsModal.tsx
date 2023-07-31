2ed45e9f2d8adb6cdf297a7959e38a1a658ff4f7
James Murdock Graham <jamesmurdockgraham@gmail.com>
2023-07-31 10:24:24 +0100

    added footnote for clarity in PR

--- apps/web/src/partials/modals/AIRecommendationsModal.tsx
+++ apps/web/src/partials/modals/AIRecommendationsModal.tsx
@@ -1,23 +1,17 @@
-import React, { useState } from 'react';
-import produce from 'immer';
+import React from 'react';
 import { web as webTypes } from '@glyphx/types';
-import { useCompletion } from 'ai/react';
 
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { WritableDraft } from 'immer/dist/internal';
-import { modalsAtom, templatesAtom } from 'state';
-import { completionAtom } from 'state/ai';
+import { useRecoilValue } from 'recoil';
+import { templatesAtom } from 'state';
 
+// FIXME: NOTE: completions require streaming + RSC in feature/next-13.4-layout - placeholder for now so Will can mark it up
 export const AIRecommendationsModal = ({ modalContent }: webTypes.AiRecommendationsModalProps) => {
-  // const completion = useRecoilValue(completionAtom);
+  //
   const completion = `It looks like the "Days Sales Outstanding" template is for you. Based on the uploaded fileStats, the
         "customer_no" column maps to the "X" key in the template, which represents the name of the customer account or
         generic billing entity. The "time" column maps to the "Y" key in the template, which represents some unit of
         time that makes sense within the billing cycle. Finally, the "DSO" column maps to the "Z" key in the template,
         which represents the average number of days that it takes a company to collect payment for a sale.`;
-
-  const setModals = useSetRecoilState(modalsAtom);
-  const [current, setCurrent] = useState(0);
 
   const formatCompletion = (completion) => {
     const regex = /"(.*?)"/g;
@@ -26,7 +20,6 @@ export const AIRecommendationsModal = ({ modalContent }: webTypes.AiRecommendati
   };
 
   const recommended = formatCompletion(completion);
-
   const { templates } = useRecoilValue(templatesAtom);
 
   return (
@@ -34,9 +27,8 @@ export const AIRecommendationsModal = ({ modalContent }: webTypes.AiRecommendati
       <div className="text-white text-xl py-4">Recommendations</div>
       {/* @ts-ignore */}
       {completion && <div className="text-white text-md">{completion}</div>}
-
       <div
-        className={`group my-8 flex shadow-sm rounded border  ${'border-transparent'} bg-secondary-space-blue hover:cursor-pointer hover:border-white p-2`}
+        className={`group my-8 flex shadow-sm rounded border border-transparent bg-secondary-space-blue hover:cursor-pointer hover:border-white p-2`}
       >
         <div className="flex-1 flex items-center justify-between border-t border-r border-b border-transparent  rounded-r-md whitespace-wrap">
           <div className="flex-1 px-4 py-2 text-sm">
