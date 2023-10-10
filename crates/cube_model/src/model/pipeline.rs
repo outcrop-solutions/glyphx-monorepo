d615d5b29f45f10e8396da3914ec4ae29d94564d
jp-burford <48831852+jp-burford@users.noreply.github.com>
2023-10-10 15:17:18 -0500

    Feature/wiring model to sandbox (#165)

--- crates/cube_model/src/model/pipeline.rs
+++ crates/cube_model/src/model/pipeline.rs
@@ -2,13 +2,12 @@ pub(crate) mod axis_lines;
 pub(crate) mod glyphs;
 pub(crate) mod pipeline_manager;
 
-use crate::assets::color::Color;
 use smaa::SmaaFrame;
 
 pub trait PipelineRunner {
     fn run_pipeline<'a>(
         &'a self,
         encoder: &'a mut wgpu::CommandEncoder,
-        smaa_frame: &SmaaFrame,
+        smaa_frame: &SmaaFrame
     );
 }
