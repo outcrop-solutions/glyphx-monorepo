518f06fe1c1e8d5afafea26ed38bee5e2491f947
JP Burford <jp@glyphx.co>
2023-10-26 12:03:37 -0400

    Added athena_connection_errors to types and cleaned up features

--- crates/types/src/common.rs
+++ crates/types/src/common.rs
@@ -1,1 +1,4 @@
+#[cfg( feature="s3_connection" )]
 pub mod s3_connection_errors;
+#[cfg( feature="athena_connection" )]
+pub mod athena_connection_errors;
--- crates/types/src/common/s3_connection_errors.rs
+++ crates/types/src/common/s3_connection_errors.rs
@@ -4,14 +4,14 @@ use crate::aws::s3_manager::ConstructorError as S3ManagerConstructorError;
 //std::fmt::Display
 #[derive(Debug)]
 pub enum ConstructorError {
-   BucketDoesNotExistsError(GlyphxErrorData), 
+   BucketDoesNotExist(GlyphxErrorData), 
    UnexpectedError(GlyphxErrorData),
 }
 
 impl ConstructorError {
     pub fn from_s3_manager_constructor_error(err: S3ManagerConstructorError) -> Self {
         match err {
-            S3ManagerConstructorError::BucketDoesNotExist(err) => Self::BucketDoesNotExistsError(err),
+            S3ManagerConstructorError::BucketDoesNotExist(err) => Self::BucketDoesNotExist(err),
             S3ManagerConstructorError::UnexpectedError(err) => Self::UnexpectedError(err),
         }
     } 
