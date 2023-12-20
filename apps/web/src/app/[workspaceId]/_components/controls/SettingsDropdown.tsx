4e3a0ce27dc712767a1342ab580a8d2a7b45b040
James Murdock Graham <jamesmurdockgraham@gmail.com>
2023-12-19 23:27:56 -0500

    trying to debounce rowids vs datagrid

--- apps/web/src/app/[workspaceId]/_components/controls/SettingsDropdown.tsx
+++ apps/web/src/app/[workspaceId]/_components/controls/SettingsDropdown.tsx
@@ -48,20 +48,11 @@ export const SettingsDropdown = () => {
           <div className="p-2">
             <Menu.Item>
               <Link
-                href={'/account/settings' as Route}
+                href={`/${workspaceId}/settings` as Route}
                 className="flex items-center w-full px-2 py-2 space-x-2 text-sm text-gray-800 rounded hover:bg-blue-600 hover:text-white group"
               >
                 <UserCircleIcon aria-hidden="true" className="w-5 h-5" />
                 <span>Account</span>
-              </Link>
-            </Menu.Item>
-            <Menu.Item>
-              <Link
-                href={'/account/billing' as Route}
-                className="flex items-center w-full px-2 py-2 space-x-2 text-sm text-gray-800 rounded hover:bg-blue-600 hover:text-white group"
-              >
-                <CreditCardIcon aria-hidden="true" className="w-5 h-5" />
-                <span>Billing</span>
               </Link>
             </Menu.Item>
           </div>
--- apps/web/src/lib/client/hooks/useDataGrid.ts
+++ apps/web/src/lib/client/hooks/useDataGrid.ts
@@ -3,8 +3,8 @@ import {useEffect, useMemo, useState} from 'react';
 import {api} from '../network';
 import {_getDataGrid, _getRowIds} from '../mutations';
 import {dataGridPayloadSelector, rowIdsAtom} from 'state';
-import {useRecoilValue} from 'recoil';
 import {debounce} from 'lodash';
+import {useRecoilValue} from 'recoil';
 
 const useDataGrid = () => {
   const [data, setData] = useState(null);
@@ -32,39 +32,36 @@ const useDataGrid = () => {
     [workspaceId, projectId, tableName]
   );
 
-  const fetchDataWithRowIds = debounce(async () => {
+  const fetchDataWithRowIds = async () => {
     console.log('fetch data row with ids debounced');
     setIsLoadingRowIds(true);
     const data = await api(fetchRowIdsConfig);
     setData(data);
     setIsLoadingRowIds(false);
-  }, 500);
+  };
 
-  useEffect(() => {
-    if (!tableName || isLoadingRowIds || !rowIds) {
-      return;
-    }
-    fetchDataWithRowIds();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [rowIds, tableName, isLoadingRowIds, fetchRowIdsConfig]);
-
-  const fetchDataWithoutRowIds = debounce(async () => {
+  const fetchDataWithoutRowIds = async () => {
     setIsLoadingDataGrid(true);
     const data = await api(fetchDataGridConfig);
-    if (!rowIds) {
-      setData(data);
-    }
+    setData(data);
     setIsLoadingDataGrid(false);
-  }, 500);
+  };
+
+  const fetchData = debounce(async () => {
+    if (!tableName) {
+      if (!rowIds && !isLoadingDataGrid) {
+        fetchDataWithoutRowIds();
+      } else if (rowIds && !isLoadingRowIds) {
+        fetchDataWithRowIds();
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, 1000);
 
   useEffect(() => {
-    if (!tableName || isLoadingDataGrid || rowIds) {
-      return;
-    }
-
-    fetchDataWithoutRowIds();
+    fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isLoadingDataGrid, tableName, fetchDataGridConfig, rowIds]);
+  }, [rowIds, tableName, isLoadingRowIds, fetchRowIdsConfig]);
 
   return {data};
 };
