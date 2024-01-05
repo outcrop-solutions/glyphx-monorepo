9d835acadc321087a7e3b8f8fbb2bd2e5597f7f9
James Murdock Graham <jamesmurdockgraham@gmail.com>
2024-01-05 08:42:21 -0500

    refactored generateFilterQuery|

--- apps/web/src/lib/client/helpers/generateFilterQuery.ts
+++ apps/web/src/lib/client/helpers/generateFilterQuery.ts
@@ -1,19 +1,47 @@
 import {BasicColumnNameCleaner} from 'fileingestion';
-import {webTypes, fileIngestionTypes} from 'types';
+import {webTypes, fileIngestionTypes, databaseTypes} from 'types';
 
-export const generateFilterQuery = (prop: webTypes.Property) => {
-  const cleaner = new BasicColumnNameCleaner();
-  const name = cleaner.cleanColumnName(prop.key);
-
+const generateSegment = (name: string, prop: webTypes.Property) => {
   switch (prop.dataType) {
     case fileIngestionTypes.constants.FIELD_TYPE.NUMBER:
-      const numFilter = prop.filter as unknown as webTypes.INumbericFilter;
-      return `${name || '-'} BETWEEN ${numFilter.min || '-'} AND ${numFilter.max || '-'}`;
+      const {min, max} = prop.filter as unknown as webTypes.INumbericFilter;
+      if (min && max && min === 0 && max === 0) {
+        return '';
+      } else {
+        return `${name || '-'} BETWEEN ${min || '-'} AND ${max || '-'}`;
+      }
     case fileIngestionTypes.constants.FIELD_TYPE.STRING:
-      const strFilter = prop.filter as unknown as webTypes.IStringFilter;
-      return `MATCH ${name || '-'} AGAINST (${strFilter.keywords.join(' ') || '-'})`;
+      const {keywords} = prop.filter as unknown as webTypes.IStringFilter;
+      if (keywords && keywords.length === 0) {
+        return '';
+      } else {
+        return `MATCH ${name || '-'} AGAINST (${keywords.join(' ') || '-'})`;
+      }
     // TODO: fileIngestionTypes.constants.FIELD_TYPE.DATE
     default:
       return '';
   }
+};
+
+const genFilter = (prop: webTypes.Property, axis: webTypes.constants.AXIS) => {
+  const cleaner = new BasicColumnNameCleaner();
+  const name = cleaner.cleanColumnName(prop.key);
+  const isZ = axis === webTypes.constants.AXIS.Z;
+  const segment = isZ ? ' AND ' : '';
+
+  const retval = generateSegment(name, prop);
+
+  return `${retval}${segment}`;
+};
+
+export const generateFilterQuery = (properties: databaseTypes.IProject['state']['properties']) => {
+  const axes = Object.values(webTypes.constants.AXIS);
+  const segments: string[] = [];
+
+  for (const axis of axes) {
+    const segment = genFilter(properties[axis], axis);
+    segments.push(segment);
+  }
+
+  return segments.join('');
 };
