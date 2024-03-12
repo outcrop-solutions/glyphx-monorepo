211bf723f69a3827540269b69ae118c678732709
JP Burford <jp@glyphx.co>
2024-03-12 12:03:28 -0400

    UPdating env for exploration.

--- apps/web/src/app/api/env/route.ts
+++ apps/web/src/app/api/env/route.ts
@@ -3,12 +3,13 @@ import fs from 'node:fs';
 
 export const runtime = 'nodejs';
 
-export async function GET(req: Request): Promise<Response | undefined> {
+export async function POST(req: Request): Promise<Response | undefined> {
   const params = await req.text();
+  console.log('params', params);
   const env = process.env.GLYPHX_ENV || 'dev';
   const processDir = process.cwd();
   const files = fs.readdirSync(processDir);
-  const queryFiles = params ? fs.readdirSync(params) : [];
+  const queryFiles = params ? fs.readdirSync(processDir + '/' + params) : [];
 
   if (env) {
     return NextResponse.json({env, processDir, files, queryFiles});
