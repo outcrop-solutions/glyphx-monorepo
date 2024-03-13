2fd5d8f089c79dd6558915e24c74776dd6f04f53
James Murdock Graham <jamesmurdockgraham@gmail.com>
2024-03-13 11:34:23 -0500

    removed testing page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,4 @@
-import {AnnotationCreatedTemplate} from 'actions/src/email/templates';
-
 // landing page
 export default async function LandingPage() {
-  return (
-    // @ts-ignore
-    <AnnotationCreatedTemplate stateName="State Name" stateImage="hello" annotation="This is the annotation" />
-  );
+  return <div></div>;
 }
