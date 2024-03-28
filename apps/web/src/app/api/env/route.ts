0d9d69efb26ace9871422ac2215a68a9f1dfe679
JP Burford <jp@glyphx.co>
2024-03-28 12:55:08 -0400

    Trying to reference index.node from API having glyph_engine_rust directly included.

--- apps/web/src/app/api/env/route.ts
+++ apps/web/src/app/api/env/route.ts
@@ -1,10 +1,16 @@
 import {NextResponse} from 'next/server';
 import fs from 'node:fs';
-
+import * as actions from 'actions';
 export const runtime = 'nodejs';
 
 export async function POST(req: Request): Promise<Response | undefined> {
   const params = await req.text();
+  let hello = '';
+  try {
+    let hello = actions.hello();
+  } catch (error) {
+    hello = 'failed to load index.node';
+  }
   console.log('params', params);
   const env = process.env.GLYPHX_ENV || 'dev';
   const processDir = process.cwd();
