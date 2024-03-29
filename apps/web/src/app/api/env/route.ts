7b8a43b21224efcbd099fc4098bf578a43ec9689
JP Burford <jp@glyphx.co>
2024-03-29 14:48:45 -0400

    Finishing cleanup

--- apps/web/src/app/api/env/route.ts
+++ apps/web/src/app/api/env/route.ts
@@ -1,11 +1,9 @@
 import {NextResponse} from 'next/server';
 export const runtime = 'nodejs';
-import * as actions from 'actions';
 
 export async function GET(req: Request): Promise<Response | undefined> {
   const env = process.env.GLYPHX_ENV || 'dev';
-  let hello = await actions.hello();
   if (env) {
-    return NextResponse.json({env, hello});
+    return NextResponse.json({env});
   }
 }
