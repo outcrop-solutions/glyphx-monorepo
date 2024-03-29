ffaa7064ee0cddaed202ab3d293c7966a350165a
JP Burford <jp@glyphx.co>
2024-03-29 14:29:04 -0400

    Cleaning up after fixing deployment

--- apps/web/src/app/api/env/route.ts
+++ apps/web/src/app/api/env/route.ts
@@ -1,16 +1,11 @@
 import {NextResponse} from 'next/server';
-import fs from 'node:fs';
 export const runtime = 'nodejs';
+import * as actions from 'actions';
 
-export async function POST(req: Request): Promise<Response | undefined> {
-  const params = await req.text();
-  console.log('params', params);
+export async function GET(req: Request): Promise<Response | undefined> {
   const env = process.env.GLYPHX_ENV || 'dev';
-  const processDir = process.cwd();
-  const files = fs.readdirSync(processDir);
-  const queryFiles = params ? fs.readdirSync(processDir + '/' + params) : [];
-
+  let hello = await actions.hello();
   if (env) {
-    return NextResponse.json({env, processDir, files, queryFiles});
+    return NextResponse.json({env, hello});
   }
 }
--- apps/web/src/app/api/test/route.ts
+++
@@ -1,8 +0,0 @@
-import {NextResponse} from 'next/server';
-import fs from 'node:fs';
-export const runtime = 'nodejs';
-import * as actions from 'actions';
-
-export async function POST(req: Request): Promise<Response | undefined> {
-  return NextResponse.json({hello: actions.hello()});
-}
