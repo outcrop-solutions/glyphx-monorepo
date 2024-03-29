62ee7d51c07d67134c25730638f0e46b43db5c7a
James Murdock Graham <jamesmurdockgraham@gmail.com>
2024-03-29 14:04:16 -0500

    added logs

--- apps/web/src/lib/client/network/reqs/callCreateModel.ts
+++ apps/web/src/lib/client/network/reqs/callCreateModel.ts
@@ -16,58 +16,65 @@ export const callCreateModel = async ({
   setResize,
   mutate,
 }) => {
-  // set initial loading state
-  setLoading(
-    produce((draft: WritableDraft<Partial<Omit<databaseTypes.IProcessTracking, '_id'>>>) => {
-      draft.processName = 'Generating Data Model...';
-      draft.processStatus = databaseTypes.constants.PROCESS_STATUS.IN_PROGRESS;
-      draft.processStartTime = new Date();
-    })
-  );
-  const cleanProject = {
-    ...project,
-    stateHistory: [],
-  };
-  // create model
-  const retval = await glyphEngine(cleanProject, payloadHash);
-  console.log({glyphengineRetval: retval});
-
-  if (!retval?.error) {
+  try {
+    // set initial loading state
     setLoading(
       produce((draft: WritableDraft<Partial<Omit<databaseTypes.IProcessTracking, '_id'>>>) => {
-        draft.processName = 'Fetching Data...';
+        draft.processName = 'Generating Data Model...';
+        draft.processStatus = databaseTypes.constants.PROCESS_STATUS.IN_PROGRESS;
+        draft.processStartTime = new Date();
       })
     );
-    // sign data urls
-    console.log({projectInput: project, payloadHash});
-    const signedUrls = await signDataUrls(project?.workspace.id, project?.id, payloadHash);
-    console.log({signedUrls});
-    if (!signedUrls?.error) {
-      setLoading({});
-      if (window?.core) {
-        setResize(150);
-        setDrawer(true);
-        console.log('calling open project');
-        window?.core?.OpenProject(
-          _createOpenProject(signedUrls as {sdtUrl: any; sgcUrl: any; sgnUrl: any}, project, session, url, true, [])
+    const cleanProject = {
+      ...project,
+      stateHistory: [],
+    };
+    // create model
+    const retval = await glyphEngine(cleanProject, payloadHash);
+    console.dir(retval, {depth: 2});
+
+    if (!retval?.error) {
+      setLoading(
+        produce((draft: WritableDraft<Partial<Omit<databaseTypes.IProcessTracking, '_id'>>>) => {
+          draft.processName = 'Fetching Data...';
+        })
+      );
+      // sign data urls
+      console.log({projectInput: project, payloadHash});
+      const signedUrls = await signDataUrls(project?.workspace.id, project?.id, payloadHash);
+      console.dir(signedUrls, {depth: 2});
+
+      if (!signedUrls?.error) {
+        setLoading({});
+        if (window?.core) {
+          setResize(150);
+          setDrawer(true);
+          console.log('calling open project');
+          window?.core?.OpenProject(
+            _createOpenProject(signedUrls as {sdtUrl: any; sgcUrl: any; sgnUrl: any}, project, session, url, true, [])
+          );
+        }
+      } else {
+        console.log('failed to open model');
+        setLoading(
+          produce((draft: WritableDraft<Partial<Omit<databaseTypes.IProcessTracking, '_id'>>>) => {
+            draft.processName = 'Failed to Open Model';
+            draft.processStatus = databaseTypes.constants.PROCESS_STATUS.FAILED;
+            draft.processEndTime = new Date();
+          })
         );
       }
     } else {
+      console.log('failed to generate model');
       setLoading(
         produce((draft: WritableDraft<Partial<Omit<databaseTypes.IProcessTracking, '_id'>>>) => {
-          draft.processName = 'Failed to Open Model';
+          draft.processName = 'Failed to Generate Model';
           draft.processStatus = databaseTypes.constants.PROCESS_STATUS.FAILED;
           draft.processEndTime = new Date();
         })
       );
     }
-  } else {
-    setLoading(
-      produce((draft: WritableDraft<Partial<Omit<databaseTypes.IProcessTracking, '_id'>>>) => {
-        draft.processName = 'Failed to Generate Model';
-        draft.processStatus = databaseTypes.constants.PROCESS_STATUS.FAILED;
-        draft.processEndTime = new Date();
-      })
-    );
+  } catch (error) {
+    console.log({error});
   }
 };
