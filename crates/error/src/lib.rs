ee33a32149ac2ea15365ceffb3ff885db4676c05
JP Burford <jp@glyphx.co>
2024-04-03 16:56:11 -0400

    Split error macro into two macros one for core and one for core_error

--- crates/error/src/lib.rs
+++ crates/error/src/lib.rs
@@ -3,4 +3,6 @@ mod error_type_parser;
 
 pub use error::GlyphxErrorData;
 pub use error_type_parser::ErrorTypeParser;
-pub use glyphx_error::GlyphxError;
+pub use glyphx_error::{GlyphxError, GlyphxCoreError};
+pub use serde_json::json;
+pub use log::{debug, error, info, trace, warn};
