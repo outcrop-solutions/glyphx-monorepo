21d06683f7412ba15e90305d9f0d4602d5e34d14
James Murdock Graham <jamesmurdockgraham@gmail.com>
2024-05-10 14:29:20 -0500

    added back test route

---
+++ apps/web/src/app/api/test/route.ts
@@ -0,0 +1,16 @@
+import {NextResponse} from 'next/server';
+import fs from 'node:fs';
+export const runtime = 'nodejs';
+
+export async function POST(req: Request): Promise<Response | undefined> {
+  const params = await req.text();
+  console.log('params', params);
+  const env = process.env.GLYPHX_ENV || 'dev';
+  const processDir = process.cwd();
+  const files = fs.readdirSync(processDir);
+  const queryFiles = params ? fs.readdirSync(processDir + '/' + params) : [];
+
+  if (env) {
+    return NextResponse.json({env, processDir, files, queryFiles});
+  }
+}
