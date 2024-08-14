d87bc196b675591033fa65518c3a97c85c844420
JP Burford <jp@glyphx.co>
2024-08-14 14:37:39 -0400

    Added get_statistics method to ModelRunner

--- crates/cube_model/src/lib.rs
+++ crates/cube_model/src/lib.rs
@@ -353,6 +353,28 @@ impl ModelRunner {
         }
     }
 
+    #[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
+    pub fn get_statistics(&self, axis: &str) -> Result<String, String> {
+        if ["x", "y", "x"].contains(&axis) {
+            return Err("Invalid axis.  Must be x, y, or z".to_string());
+        }
+        //NOTE: You should have seen this before.  Y and Z are flipped in the model, so we need to
+        //flip them back to return the correct values to the client.
+        let axis = match axis {
+            "x" => "x",
+            "y" => "z",
+            "z" => "y",
+            _ => "x",
+        };
+        let mut dm = self.data_manager.borrow();
+      
+        let result = dm.get_stats(axis);
+        match result {
+            Ok(stats) => Ok(serde_json::to_string(&stats).unwrap()),
+            Err(e) => Err(serde_json::to_string(&e).unwrap()),
+        }
+    }
+
     ///Adding a glyph will update internal state but it
     ///will not emit any redraw events.
     #[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
@@ -875,6 +897,7 @@ impl ModelRunner {
                                     let mut cf = config.borrow_mut();
                                     cf.color_flip = !cf.color_flip;
                                 }
+                            }
                             state.update_config();
                             unsafe {
                                 let event = ModelEvent::Redraw;
