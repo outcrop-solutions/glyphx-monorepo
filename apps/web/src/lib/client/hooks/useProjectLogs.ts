86ad927c7784504a0a460fb9ebfa91a3d92b10d0
James Murdock Graham <jamesmurdockgraham@gmail.com>
2024-09-17 16:22:00 -0400

    fix typo

--- apps/web/src/lib/client/hooks/useProjectLogs.ts
+++ apps/web/src/lib/client/hooks/useProjectLogs.ts
@@ -8,7 +8,6 @@ const useProjectLogs = () => {
   const {projectId} = params as {projectId: string};
   const [data, setData] = useState<any>(null);
   const [error, setError] = useState(null);
-  ` d`;
   useEffect(() => {
     const getProjectLogData = async (id) => {
       const retval = await getProjectLogs(id);
