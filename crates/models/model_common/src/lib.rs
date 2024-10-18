68376a29895f96780e9e4fdc133fbf412dd4915d
JP Burford <jp@glyphx.co>
2024-10-18 15:29:04 -0400

    Added uniforms, colors and interpolation to model_common

--- crates/models/model_common/src/lib.rs
+++ crates/models/model_common/src/lib.rs
@@ -3,9 +3,15 @@ mod stats;
 mod glyph;
 mod wgpu;
 mod camera;
+mod uniforms;
+mod colors;
+mod interpolation;
 
 pub use stats::Stats;
 pub use glyph::Glyph;
 pub use vectors::*;
 pub use camera::*;
 pub use wgpu::wgpu_manager::WgpuManager;
+pub use uniforms::*;
+pub use colors::*;
+pub use interpolation::*;
